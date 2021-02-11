43ca524faa524ee17d3ebd4737150587e26b49ce
Victor Brito <victor.brito@izap.com.br>
2021-02-11 17:03:39 -0300

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $now = new \DateTime();
+
+        DB::table('products')->insert([
+            'name' => 'Pizza',
+            'price' => 40,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Parmegiana de carne',
+            'price' => 70,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Lasanha de frango',
+            'price' => 50,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Hambúrguer',
+            'price' => 25,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
     }
 }
