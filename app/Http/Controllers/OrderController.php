d2ecad1171962552a10a0221b09c7e669332f8b3
Victor Brito <victor.brito@izap.com.br>
2021-02-12 10:04:52 -0300

    Controllers - initial configuration

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index(Request $request)
+    {
+
+    }
+
+    public function show(Request $request)
+    {
+
+    }
+
+    public function changeStatus(Request $request)
+    {
+
+    }
+
+    public function finish(Request $request)
+    {
+
+    }
+}
---
+++ app/Http/Controllers/OrderProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderProductController extends Controller
+{
+    public function addProduct(Request $request)
+    {
+
+    }
+
+    public function removeProduct(Request $request)
+    {
+
+    }
+}
